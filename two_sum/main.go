c392b1782c55c9fa010ae08f32d29b91b53587ee
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2024-01-01 23:22:05 +0900

    1. Two Sum

---
+++ two_sum/main.go
@@ -0,0 +1,48 @@
+package main
+
+import "fmt"
+
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+
+Example 1:
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+Example 2:
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+
+Example 3:
+Input: nums = [3,3], target = 6
+Output: [0,1]
+
+Constraints:
+2 <= nums.length <= 10^4
+-10^9 <= nums[i] <= 10^9
+-10^9 <= target <= 10^9
+Only one valid answer exists.
+*/
+func twoSum(nums []int, target int) []int {
+	numMap := make(map[int]int)
+	for i, num := range nums {
+		complement := target - num
+		if j, ok := numMap[complement]; ok {
+			return []int{j, i}
+		}
+		numMap[num] = i
+	}
+	return nil
+}
+
+func main() {
+	result1 := twoSum([]int{2, 7, 11, 15}, 9)
+	fmt.Println(result1) // Expected Output: [0 1]
+	result2 := twoSum([]int{3, 2, 4}, 6)
+	fmt.Println(result2) // Expected Output: [1 2]
+	result3 := twoSum([]int{3, 3}, 6)
+	fmt.Println(result3) // Expected Output: [0 1]
+}
