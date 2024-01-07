7ab83caed4714ba8eb6fa9a3fb4265d528a82112
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2024-01-07 21:33:02 +0900

    35. Search Insert Position

--- problems/search_insert_position/main.go
+++ problems/search_insert_position/main.go
@@ -28,8 +28,23 @@ nums contains distinct values sorted in ascending order.
 */
 
 func searchInsert(nums []int, target int) int {
-	// TODO: implement
-	return 0
+	left, right := 0, len(nums)-1
+
+	for left <= right {
+		mid := left + (right-left)/2
+
+		if nums[mid] == target {
+			return mid
+		}
+
+		if nums[mid] < target {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+
+	return left
 }
 
 func main() {
