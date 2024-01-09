bece94da4ccb6b20236621924da57a2220b740db
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2024-01-09 22:40:34 +0900

    66. Plus One

--- problems/plus_one/main.go
+++ problems/plus_one/main.go
@@ -36,12 +36,24 @@ digits does not contain any leading 0's.
 */
 
 func plusOne(digits []int) []int {
-	// TODO: implement
-	return []int{}
+	n := len(digits)
+
+	for i := n - 1; i >= 0; i-- {
+		if digits[i] < 9 {
+			digits[i]++
+			return digits
+		}
+		digits[i] = 0
+	}
+
+	digits = append([]int{1}, digits...)
+
+	return digits
 }
 
 func main() {
-	fmt.Println(plusOne([]int{1, 2, 3}))    // Output: [1,2,4]
-	fmt.Println(plusOne([]int{4, 3, 2, 1})) // Output: [4,3,2,2]
-	fmt.Println(plusOne([]int{9}))          // Output: [1,0]
+	// fmt.Println(plusOne([]int{1, 2, 3}))    // Output: [1,2,4]
+	// fmt.Println(plusOne([]int{4, 3, 2, 1})) // Output: [4,3,2,2]
+	// fmt.Println(plusOne([]int{9}))          // Output: [1,0]
+	fmt.Println(plusOne([]int{1, 9}))          // Output: [1,0]
 }
