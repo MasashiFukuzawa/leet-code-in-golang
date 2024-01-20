880b0b9f61225296f4423848617c4db1c70d908d
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2024-01-20 20:38:33 +0900

    104. Maximum Depth of Binary Tree

---
+++ problems/maximum_depth_of_binary_tree/main.go
@@ -0,0 +1,59 @@
+package main
+
+import "fmt"
+
+// https://leetcode.com/problems/maximum-depth-of-binary-tree/
+
+/*
+Given the root of a binary tree, return its maximum depth.
+A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+Example 1:
+Input: root = [3,9,20,null,null,15,7]
+Output: 3
+
+Example 2:
+Input: root = [1,null,2]
+Output: 2
+
+Constraints:
+The number of nodes in the tree is in the range [0, 104].
+-100 <= Node.val <= 100
+*/
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	left := maxDepth(root.Left)
+	right := maxDepth(root.Right)
+
+	if left > right {
+		return left + 1
+	} else {
+		return right + 1
+	}
+}
+
+func main() {
+	node1 := &TreeNode{3, &TreeNode{9, nil, nil}, &TreeNode{20, &TreeNode{15, nil, nil}, &TreeNode{7, nil, nil}}}
+	fmt.Println(maxDepth(node1)) // Output: 3
+
+	node2 := &TreeNode{1, nil, &TreeNode{2, nil, nil}}
+	fmt.Println(maxDepth(node2)) // Output: 2
+}
